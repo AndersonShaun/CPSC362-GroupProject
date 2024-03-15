5e977e7faaaaf1c7788f5627b4ec0d87573369a9
Shaun Anderson <craniumkid89@gmail.com>
2024-03-15 10:18:04 -0700

    Rough draft of home page.

--- task-management-system/src/App.js
+++ task-management-system/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import './components/column.css';
 import './App.css';
 
-function App() {
+// Back-end starts here
+// Database
+// const initialTasks = {
+//   ToDo: ["Task 1", "Task 2"],
+//   InProgress: ["Task 3"],
+//   Completed: [],
+//   Dropped: [],
+// };
+
+function Task({ tasks, status }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task">
+      <h2>{status}</h2>
+      {Object.keys(tasks).map((status) => (
+        <div key={status} className="task-column-list">
+          <h2>{status}</h2>
+          {/* {Object.keys(tasks[status].map(task, index) => (
+            <div key={index} className="task"{task}></div>
+          ))} */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const initialTasks = {
+    ToDo: ["Task 1", "Task 2"],
+    InProgress: ["Task 3"],
+    Completed: [],
+    Dropped: [],
+  };
+
+  return (
+    <div className="Kanban-board">
+      <div className="task-column-list">
+        <Task tasks={initialTasks} status="ToDo" />
+    
+      </div>
+      
+      {/* <div className="column">
+        <h2>In-Progress</h2>
+      </div>
+
+      
+      <div className="column">
+        <h2>Completed</h2>
+      </div>
+      
+      
+      <div className="column">
+        <h2>Dropped</h2>
+      </div> */}
+      
     </div>
   );
 }
