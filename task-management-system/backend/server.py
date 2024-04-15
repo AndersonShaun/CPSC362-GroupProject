e7dce75857fa18ff8ea63a05b3db56b88207d8d2
Shaun Anderson <craniumkid89@gmail.com>
2024-04-14 17:48:17 -0700

    Update functionality added along with comments.

--- task-management-system/backend/server.py
+++ task-management-system/backend/server.py
@@ -13,17 +13,13 @@ initialTasks = {
     'Dropped': [],
   }
 
-# # Configure PyMongo
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/"
-# mongo = MongoClient(app.config["MONGO_URI"])
-# db = mongo.dbname
 
 # Attempt to connect to the MongoDB server
 client = MongoClient('localhost', 27017)
 
 # Access server information to verify the connection
 server_info = client.server_info()
-print("Successfully connected to MongoDB server:", server_info)
+# print("Successfully connected to MongoDB server:", server_info)
 
 # Access a specific database and collection to further verify the connection
 db = client['Tasks']
@@ -32,21 +28,11 @@ inprogress_collection = db['In-Progress']
 completed_collection = db['Completed']
 dropped_collection = db['Dropped']
 
-# Define the Task model
-# class Task:
-#     def __init__(self, title, description=None):
-#         self.title = title
-#         self.description = description
-
-#     def to_dict(self):
-#         return {
-#             'title': self.title,
-#             'description': self.description
-#         }
 
 # API endpoint to get all tasks (To-Do)
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
+    # Get all of the collections in the database
     todo_tasks = list(todo_collection.find())
     inprogress_tasks = list(inprogress_collection.find())
     completed_tasks = list(completed_collection.find())
@@ -65,15 +51,11 @@ def get_tasks():
     tasks_data = {
         'Dropped': dropped_tasks,
         'Completed': completed_tasks,
-        'In Progress': inprogress_tasks,
+        'In-Progress': inprogress_tasks,
         'To-Do': todo_tasks
     }
     return jsonify(tasks_data)
 
-# @app.route('/tasks/todo', methods=['GET'])
-# def get_tasks_todo():
-#     tasks = db.tasks.find({'status': 'To-Do'})
-#     return jsonify([task for task in tasks])
 
 # API endpoint to create a new task in To-Do
 @app.route('/api/tasks', methods=['POST'])
@@ -93,87 +75,103 @@ def create_task():
         '_id': str(task_id)
     }
 
-    # task_id = todo_collection.insert_one(task_data).inserted_id
-
     return jsonify(new_task_data), 201
 
 
-# API endpoint to create a new task (In Progress)
-# @app.route('/tasks/inprogress', methods=['POST'])
-# def add_task_in_progress():
-#     data = request.get_json()
-#     task = Task(data.get('title', ''), data.get('description', ''))
-#     task_id = db.tasks.insert_one({**task.to_dict(), 'status': 'In Progress'}).inserted_id
-#     return jsonify({'id': str(task_id), **task.to_dict(), 'status': 'In Progress'}), 201
 
-# API endpoint to update an existing task (Completed)
+# API endpoint to move an existing task (Completed)
 @app.route('/api/tasks/<task_id>', methods=['PUT'])
 def move_task(task_id):
     try:
-        column = request.json.get('column')
-        print("Received column:", column)  # Debugging statement
+        # Receive the 'column' variable passed in through PUT request
+        destination_column_name = request.json.get('column')
+        print("Received Destination column:", destination_column_name)  # Debugging statement
+        source_column_name = request.args.get('sourceColumn')
+        print("Received Source column: ", source_column_name)
+
+        # Check if the provided column is valid
+        if destination_column_name not in ['To-Do', 'In-Progress', 'Completed', 'Dropped']:
+            print(destination_column_name, ": does not match")
+            return jsonify({'error': 'Invalid column name'}), 400
+        
+        
+        # Determine the source collection based on the current column name
+        source_collection = db[source_column_name]
+        # Determine the destination collection based on the column name
+        destination_collection = db[destination_column_name]
+        # Find the task with the specified id
+        task = source_collection.find_one({'_id': ObjectId(task_id)})
+
+        print(task)
+        
+        # Check if the task exists
+        if not task:
+            return jsonify({'error': 'Task not found'}), 404
+        
+        
+        # Update the task's column in the database
+        source_collection.update_one({'_id': ObjectId(task_id)}, {'$set': {'column': destination_column_name}})
+
+        # Find the task by its ID and get its document
+        task_doc = source_collection.find_one({'_id': ObjectId(task_id)})
+
+        # Insert the task into the destination collection
+        destination_collection.insert_one(task_doc)
+
+        # Delete the task from the source collection
+        source_collection.delete_one({'_id': ObjectId(task_id)})
+
+        return jsonify({'success': True}), 200
         # Rest of the function
     except Exception as e:
         print("Error:", e)
         return jsonify({'success': False}), 400
 
-    # Check if the provided column is valid
-    if column not in ['To-Do', 'In-Progress', 'Completed', 'Dropped']:
-        return jsonify({'error': 'Invalid column name'}), 400
-    
-    # Find the task in the database by its ID
-    task = todo_collection.find_one({'_id': ObjectId(task_id)})
-
-    print(task)
-    
-    # Check if the task exists
-    if not task:
-        return jsonify({'error': 'Task not found'}), 404
-    
-    # Update the task's column in the database
-    todo_collection.update_one({'_id': ObjectId(task_id)}, {'$set': {'column': column}})
-    
-    # Determine the destination collection based on the column name
-    destination_collection = db[column]
-
-    # Find the task by its ID and get its document
-    task_doc = todo_collection.find_one({'_id': ObjectId(task_id)})
-
-    # Insert the task into the destination collection
-    destination_collection.insert_one(task_doc)
-
-    # Delete the task from the original collection
-    todo_collection.delete_one({'_id': ObjectId(task_id)})
-
-    return jsonify({'success': True}), 200
     
 
-# # API endpoint to update an existing task (Completed)
-# @app.route('/tasks/completed/<task_id>', methods=['PUT'])
-# def update_task_completed(task_id):
-#     data = request.get_json()
-#     db.tasks.update_one({'_id': ObjectId(task_id)}, {'$set': {**data, 'status': 'Completed'}})
-#     task = db.tasks.find_one({'_id': ObjectId(task_id)})
-#     return jsonify(task) if task else jsonify({'message': 'Task not found'}), 404
 
 # API endpoint to delete a task (Dropped)
 @app.route('/api/tasks/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
+    
+    
     try:
         # Convert the task_id to ObjectId
         task_id = ObjectId(task_id)
+        print(task_id)
 
+        # Get the passed in 'column' attribute and set it to the respective collection
+        column_name = request.args.get('collection')
+        delete_from_column = db[column_name]
+        print(delete_from_column)
+        
         # Delete the task from the database based on the task_id
-        # Example deletion logic for 'To-Do' collection:
-        todo_collection.delete_one({'_id': task_id})
+        delete_from_column.delete_one({'_id': task_id})
 
         return jsonify({'success': True}), 200
     except Exception as e:
+        
         return jsonify({'success': False, 'error': str(e)}), 500
-# @app.route('/tasks/dropped/<task_id>', methods=['DELETE'])
-# def delete_task_dropped(task_id):
-#     result = db.tasks.delete_one({'_id': ObjectId(task_id)})
-#     return jsonify({'message': 'Task dropped'}) if result.deleted_count > 0 else jsonify({'message': 'Task not found'}), 404
+
+# API endpoint to update a task
+@app.route('/api/tasks/<task_id>', methods=['PATCH'])
+def update_task(task_id):
+    try:
+        # Get the passed in parameter 'taskColumn'
+        destination_column_name = request.args.get('taskColumn')
+        print("Received Destination column:", destination_column_name)  # Debugging statement
+        # Get the updated task data from the request body
+        updated_task_data = request.json
+        print("Updated task data:", updated_task_data)
+        collection = db[destination_column_name]
+
+        # Update the task in the database
+        collection.update_one({'_id': ObjectId(task_id)}, {'$set': updated_task_data})
+
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- task-management-system/src/App.js
+++ task-management-system/src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 import Modal from 'react-modal';
 import axios from 'axios';
 
-function TaskColumn({ tasks, colName, onTaskClick, updateTasks}) {
+// Component for rendering a column of tasks
+function TaskColumn({ tasks, colName, onTaskClick, updateFrontEndTasks}) {
+  // State variables
   const [addTaskModalIsOpen, setAddTaskModalIsOpen] = useState(false);
 
+  // Handle events when Add Task button is clicked
   const handleAddTaskClick = () => {
     console.log('Add Task button clicked');
     setAddTaskModalIsOpen(true);
   };
 
+  // Close the Add Task Modal
   const closeAddTaskModal = () => {
     setAddTaskModalIsOpen(false);
   }
 
+  // Function to add a new task to the To-Do column
   const appendTask = async (event) => {
     event.preventDefault();
+    // Get the values from the input fields
     const taskName = document.getElementById('taskName').value;
     const taskDescription = document.getElementById('taskDescription').value;
     const taskColumn = colName;
@@ -35,7 +41,8 @@ function TaskColumn({ tasks, colName, onTaskClick, updateTasks}) {
       console.log('Task added successfully:', response.data.name);
       console.log('colName: ', colName);
       
-      updateTasks(colName, response.data, "add");
+      // Add the task to the frontend for rendering
+      updateFrontEndTasks(colName, response.data, "add");
       // setInitialTasks(updatedTasks);
 
       setAddTaskModalIsOpen(false); // Close the modal
@@ -44,15 +51,17 @@ function TaskColumn({ tasks, colName, onTaskClick, updateTasks}) {
       console.error('Error adding task:', error);
       // Optionally, display an error message to the user
     }
-    // Add the task to the data base
   }
   
   return (
     <div className="column">
+      {/* Render the "Add Task" button only for the "To-Do" column */}
       {colName === "To-Do" && (
         <button className ="add-button" onClick={handleAddTaskClick}>Add Task</button>
       )}
+      {/* Render the column header */}
       <h2>{colName}</h2>
+      {/* Render each task in the column */}
       {tasks.map((task, index) => (
         <div
           key={index}
@@ -62,6 +71,7 @@ function TaskColumn({ tasks, colName, onTaskClick, updateTasks}) {
           <h2>{task.name}</h2>
         </div>
       ))}
+      {/* Add Task Modal */}
       <Modal isOpen={addTaskModalIsOpen} onRequestClose={closeAddTaskModal}>
         <h2>Add New Task</h2>
         <form>
@@ -78,14 +88,17 @@ function TaskColumn({ tasks, colName, onTaskClick, updateTasks}) {
 }
 
 function App() {
+  // Various state variables
   const [initialTasks, setInitialTasks] = useState({});
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [taskInfoModalIsOpen, setTaskInfoModalIsOpen] = useState(false);
   const [moveModalIsOpen, setMoveModalIsOpen] = useState(false);
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
   const [destinationColumn, setDestinationColumn] = useState('');
-
+  const [originalTask, setOriginalTask] = useState(null);
   const [selectedTask, setSelectedTask] = useState(null);
 
   useEffect(() => {
+    console.log('Component rendered');
     // Fetch initial task data from the backend when the component mounts
     axios.get('http://127.0.0.1:5000/api/tasks')
       .then(response => {
@@ -96,131 +109,224 @@ function App() {
       });
   }, []); // Empty dependency array ensures that the effect runs only once when the component mounts
 
-  const handleTaskClick = (task) => {
-    
-    // To-Do: Get information about the Task and display it on screen
-    console.log(task);
-    setSelectedTask(task);
-    setModalIsOpen(true);
-    console.log('Clicked task:', task.name);
-    // console.log('setModalIsOpen: ', modalIsOpen);
+  const openEditModal = () => {
+    // Show the Edit modal on screen
+    setEditModalIsOpen(true);
   }
 
   const openMoveModal = () => {
+    // Show the Move Modal on screen
     setMoveModalIsOpen(true);
-    // console.log("setMoveModalIsOpen", moveModalIsOpen);
   }
 
   const closeMoveModal = () => {
+    // Close the Move Modal
     setMoveModalIsOpen(false);
-    // console.log("setMoveModalIsOpen", moveModalIsOpen);
   }
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-    // console.log("setModalIsOpen", modalIsOpen);
+  const closeEditModal = () => {
+    // Restore the task's values to their original values
+    setSelectedTask(originalTask);
+    // Close the Edit Modal
+    setEditModalIsOpen(false);
   }
 
-  const updateTasks = (columnName, task, action) => {
+  const closeTaskInfoModal = () => {
+    // Close the TaskInfo Modal
+    setTaskInfoModalIsOpen(false);
+  }
+
+  const updateFrontEndTaskColumns = (columnName, task, action) => {
     setInitialTasks(prevTasks => {
+      // Create a copy of the previous tasks object to avoid mutating state directly
       const updatedTasks = { ...prevTasks };
-      // console.log(updatedTasks);
-      // console.log(updatedTasks[columnName]);
-      // console.log(newTask)
-      // console.log(newTask.name)
-      console.log(columnName)
+      // Create an updated task object with the new column name
+      const updatedTask = {...task, column: columnName };
+      // Update the tasks based on the specified action
       if (action === "add") {
-        updatedTasks[columnName] = [...prevTasks[columnName], task];
+        updatedTasks[columnName] = [...prevTasks[columnName], updatedTask];
+        console.log("added task to: ", columnName);
       } else if (action === "delete") {
+        // Remove the task with the specified ID from the specified column
         updatedTasks[columnName] = prevTasks[columnName].filter(t => t._id !== task._id);
+        console.log("deleted task from: ", columnName);
+      } else if (action === "update") {
+        // Update the specified column with the updated task
+        updatedTasks[columnName] = [updatedTask];
+        console.log("Updated: ", updatedTasks[columnName]);
       }
-      
       return updatedTasks;
     });
   }
 
-  const handleDeleteTask = async (taskToDelete) => {
+  // Handles events when a task is clicked
+  const handleTaskClick = (task) => {
+    // Set the selected task to the clicked task
+    setSelectedTask(task);
+    
+    // Save a copy of the clicked task for live editing
+    setOriginalTask({ ...task });
+    setTaskInfoModalIsOpen(true);
+    console.log('Clicked task:', task.name, " ID: ", task._id);
+  }
+
+  // Set State to contain the column to move to
+  const handleColumnChange = (column) => {
+    setDestinationColumn(column);
+  }
+
+  // Handle the deletion of tasks
+  const handleDeleteTask = async (taskToDelete, deleteFromColumn) => {
     try {
-
-      updateTasks(taskToDelete.column, taskToDelete, "delete");
-
       // Send a DELETE request to the backend API to delete the selected task
-      await axios.delete(`http://127.0.0.1:5000/api/tasks/${selectedTask._id}`);
+      await axios.delete(`http://127.0.0.1:5000/api/tasks/${taskToDelete._id}?collection=${deleteFromColumn}`);
 
+      // Update the frontend task columns by removing the deleted task
+      updateFrontEndTaskColumns(taskToDelete.column, taskToDelete, "delete");
+      
       // Close the modal after successful deletion
-      closeModal();
+      closeTaskInfoModal();
     } catch (error) {
       console.error('Error deleting task:', error);
       // Handle error
     }
   }
 
-  const handleColumnChange = (column) => {
-    console.log("Before setDestinationColumn: ", column);
-    setDestinationColumn(column);
-    // console.log("Before setDestinationColumn: ", e);
-  }
-
-  const handleMoveTask = async(taskToMove, destColumn) => {
-    console.log(destColumn);
-    console.log(taskToMove);
-    console.log(selectedTask._id);
-    if (selectedTask._id === taskToMove._id) {
-      console.log("Task IDs are the same!");
-    }
+  // Handles moving a task from one column to another
+  const handleMoveTask = async(taskToMove, destColumn, sourceColumn) => {
+    console.log("column to add to:", destColumn);
+    console.log("column to delete from: ", sourceColumn);
+    
     try {
-      // updateTasks(taskToMove.column, taskToMove, "remove");
+      // Define the new column data for the task
       const moveToColumn = {
         column: destColumn
       }
       console.log("Moving task to: ", moveToColumn);
 
-      await axios.put(`http://127.0.0.1:5000/api/tasks/${taskToMove._id}`, moveToColumn);
+      // Send a PUT request to the backend API to update the task's column
+      await axios.put(`http://127.0.0.1:5000/api/tasks/${taskToMove._id}?sourceColumn=${sourceColumn}`, moveToColumn);
       
-      // updateTasks(taskToMove.column, taskToMove, "delete");
-      // updateTasks(taskToMove.column, taskToMove, "add");
+      // Update the frontend task columns
+      updateFrontEndTaskColumns(sourceColumn, taskToMove, "delete");
+      updateFrontEndTaskColumns(destColumn, taskToMove, "add");
 
-      closeModal();
+      // Close the move modal and task info modal after successful task move
+      closeMoveModal();
+      closeTaskInfoModal();
       
     } catch (error) {
       console.error('Error moving task:', error);
     }
+  }
+
+  // Handles updating the attributes of a task
+  const handleUpdateTask = async(taskToUpdate, newTaskName, newTaskDescription, column) => {
+    console.log(taskToUpdate);
+    console.log("handleUpdateTask column: ", column);
+    console.log("name to change: ", newTaskName);
+    console.log("description to change: ", newTaskDescription);
+  
+    try {
+      // Define the column data for the task
+      const taskColumn = {
+        column: column
+      }
+
+      // Send a PATCH request to update the task with new attributes
+      await axios.patch(`http://127.0.0.1:5000/api/tasks/${selectedTask._id}?taskColumn=${column}`, {
+          name: newTaskName,
+          description: newTaskDescription
+      }, taskColumn);
+
+      // Update the frontend task columns to reflect the changes
+      updateFrontEndTaskColumns(selectedTask.column, selectedTask, "update");
+
+      // Close the various modals
+      closeEditModal();
+      closeTaskInfoModal();
+    } catch (error) {
+        console.error('Error updating task:', error);
+    }
     
   }
 
+  // Render the Kanban board, consisting of TaskColumn components for each column
   return (
     <div className="Kanban-board">
+      {/* Map over each column in initialTasks and render a TaskColumn component */}
       {Object.keys(initialTasks).reverse().map((columnName, index) => (
         <div key={index} className="task-column-list">
+          {/* Render a TaskColumn component */}
           <TaskColumn
             tasks={initialTasks[columnName]}
             colName={columnName}
             onTaskClick={handleTaskClick}
-            updateTasks={updateTasks}
+            updateFrontEndTasks={updateFrontEndTaskColumns}
           />
         </div>
       ))}
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+
+      {/* Task Info Modal*/}
+      <Modal isOpen={taskInfoModalIsOpen} onRequestClose={closeTaskInfoModal}>
+        {/* Render task information and various buttons*/}
         <h2>{selectedTask && selectedTask.name}</h2>
         <p>{selectedTask && selectedTask.description}</p>
-        <button className="cancel-button" onClick={closeModal}>Close</button>
-        <button className="delete-button" onClick={() => handleDeleteTask(selectedTask)}>Delete</button>
+        <button className="cancel-button" onClick={closeTaskInfoModal}>Close</button>
+        <button className="delete-button" onClick={() => handleDeleteTask(selectedTask, selectedTask.column)}>Delete</button>
         <button className="move-button" onClick={openMoveModal}>Move</button>
+        <button className="edit-button" onClick={openEditModal}>Edit</button>
       </Modal>
+
+      {/* Move Modal*/}
       <Modal isOpen={moveModalIsOpen} onRequestClose={() => setMoveModalIsOpen(false)}>
+        {/* Render move modal content */}
         <h2>Move this task to where?</h2>
         <p>Select where to move task: </p>
+        {/* Dropdown for selecting destination column */}
         <select onChange={(e) => handleColumnChange(e.target.value)}>
           <option value =""></option>
-          <option value ="In-Progress"> In Progress</option>
           <option value ="To-Do">To-Do</option>
+          <option value ="In-Progress"> In Progress</option>
           <option value ="Completed">Completed</option>
           <option value ="Dropped">Dropped</option>
         </select>
         <button className="cancel-button" onClick={closeMoveModal}>Cancel</button>
-        <button className="move-button" onClick={() => handleMoveTask(selectedTask, destinationColumn)}>Move Task</button>
+        <button className="move-button" onClick={() => handleMoveTask(selectedTask, destinationColumn, selectedTask.column)}>Move Task</button>
       </Modal>
       
+      {/* Edit Modal*/}
+      <Modal isOpen={editModalIsOpen} onRequestClose={() => setEditModalIsOpen(false)}>
+        <h2>Edit</h2>
+        <div>
+          <label htmlFor="editName">Task Name: </label>
+          <input
+            type="text"
+            id="editName"
+            defaultValue={selectedTask?.name}
+            onChange={(e) => {
+              console.log('New name: ', e.target.value);
+              setSelectedTask({
+                ...selectedTask,
+                name: e.target.value
+              });
+            }}/>
+          <label htmlFor="editName">Task Description: </label>
+          <input
+            type="text"
+            id="editDescription"
+            defaultValue={selectedTask?.description}
+            onChange={(e) => {
+              console.log('New description: ', e.target.value);
+              setSelectedTask({
+                ...selectedTask,
+                description: e.target.value
+              });
+            }}/>
+        </div>
+        <button className="cancel-button" onClick={closeEditModal}>Cancel</button>
+        <button className="update-button" onClick={() => handleUpdateTask(selectedTask, selectedTask.name, selectedTask.description, selectedTask.column)}>Update</button>
+      </Modal>  
     </div>
   );
 }
